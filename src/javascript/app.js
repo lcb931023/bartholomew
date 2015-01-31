db59997c952c6fa9ed84af1110e35d29cbef41cf
lcb931023 <lcb931023@gmail.com>
2015-01-31 17:59:53 -0500

    add pagepiling as a module

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -2,6 +2,6 @@ console.log('app.js loaded!');
 
 var $ = require('jquery');
 var _ = require('underscore');
-
+var PagePiling = require('pagepiling');
 var ViewHeader = require('./view/header');
 var viewHeader = new ViewHeader({ el: '#header' });
